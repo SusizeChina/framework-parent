74e95fc292117f8bf45b4e98b11526f54facc957
架构-李惠权 <li_hq@hisuntech.com>
2019-08-29 22:37:29 +0800

    add: ignore file

---
+++ spring-cloud-framework/spring-cloud-stream/spring-cloud-stream-rabbit-producer/src/test/java/com/lhq/cloud/stream/rabbit/producer/SpringCloudStreamRabbitProducerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lhq.cloud.stream.rabbit.producer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudStreamRabbitProducerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
